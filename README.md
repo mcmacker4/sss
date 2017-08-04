80198131c74d9cc7b2890eec0e527110523f28f0
mcmacker4 <mcmacker4@gmail.com>
2017-08-04 18:22:23 +0100

    First and complete version of sss on front-end.

---
+++ README.md
@@ -0,0 +1,11 @@
+SuperSecretSettings
+=
+
+[Original project](https://github.com/pagoru/sss) by [@pagoru](https://github.com/pagoru)
+
+This is a port to the front-end in order to avoid possible man-in-the-middle attacks.
+Original version sends `secret` and `service` to the server and the server calculates the password.
+
+In this version passwords are calculated in the client's side with no communication with the server.
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import copy from 'copy-to-clipboard';
+import Favicon from 'react-favicon';
+import SuperSecretSettings from 'super-secret-settings';
+
+class App extends Component {
+
+    constructor() {
+        super();
+        this.sss = new SuperSecretSettings({ masterPassword: '' });
+        this.state = {
+            hexSeed: this.sss.getMasterPasswordHash().substring(0, 8),
+            secret: '',
+            service: '',
+        };
+    }
+
+    copyToClipboard = () => {
+        console.log('Copied to clipboard');
+        const password = this.sss.generatePassword({serviceName: this.state.service});
+        copy(password);
+    };
+
+    onSecretChange = (event) => {
+        this.sss = new SuperSecretSettings({ masterPassword: event.target.value });
+        this.setState({
+            secret: event.target.value,
+            hexSeed: this.sss.getMasterPasswordHash().substring(0,8)
+        });
+    };
+
+    onServiceChange = (event) => {
+        this.setState({
+            service: event.target.value
+        })
+    };
+
+    render() {
+        return (
+            <div className="robot">
+                <label className="noselect" id="hexadecimalSeed">{this.state.hexSeed}</label>
+                <div className="bubble" id="bocata">click to copy</div>
+                <div className="inputs">
+                    <input id="secret" placeholder="secret" type="password" autoFocus="autofocus" value={this.state.secret} onChange={this.onSecretChange} />
+                    <input id="service" placeholder="service" value={this.state.service} onChange={this.onServiceChange}/>
+                </div>
+                <img id="robotRock" src={'https://robohash.org/' + this.state.hexSeed} alt="robot" />
+                <button className="password" id="password" onClick={this.copyToClipboard}/>
+                <Favicon url={'https://robohash.org/' + this.state.hexSeed} />
+            </div>
+        );
+    }
+}
+
+export default App;
